cf20ed96dfcf53ed1d2ba2ff24f004e324ecb20a
fanfzj <1009137312@qq.com>
2015-09-08 09:55:03 +0800

    新增城市爬取，存入数据库

--- tutorial/tutorial/spiders/a51newren.py
+++ tutorial/tutorial/spiders/a51newren.py
@@ -1,6 +1,23 @@
 # -*- coding: utf-8 -*-
 import scrapy
+
 from tutorial.items import DmozItem
+
+
+def change_word(s):
+    sum = 0
+    for i in s[0]:
+        sum += 1
+    ss2 = ''
+
+    for i in range(0, sum):
+        if (s[0][i] == u'\u2014'):
+            continue
+        ss2 += s[0][i]
+
+    s = ss2
+    print s
+    return s
 
 class A51newrenSpider(scrapy.Spider):
     name = "51newren"
@@ -8,8 +25,8 @@ class A51newrenSpider(scrapy.Spider):
     start_urls = (
         'http://www.51newren.com/',
     )
-
     def parse(self, response):
+        sel = scrapy.Selector(response)
         item = DmozItem()
         sel=scrapy.Selector(response)
 
